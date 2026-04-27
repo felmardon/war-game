eeafce09097358c2d91c6679829a8ac4201a079f
Felipe Mardones <mardones.felipe30@gmail.com>
2026-04-27 19:47:49 -0400

    Enhance error handling and usage information in Driver and MainApp classes

--- src/main/java/app/Driver.java
+++ src/main/java/app/Driver.java
@@ -1,31 +1,44 @@
 package app;
 
-//import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.xml.bind.JAXBException;
-
+/**
+ * Entry point for the War Game Simulation application.
+ *
+ * @author Felipe Mardones
+ */
 public class Driver {
+
 	public static void main(String[] args) {
+		// Validate command-line arguments
+		if (args.length == 0) {
+			printUsage();
+			System.exit(1);
+		}
+
 		try {
 			new MainApp(args[0]);
 		}
-		
-		catch (JAXBException jaxbException) {
-			System.err.println("Error! JAXBException caught");
+		catch (FileNotFoundException e) {
+			System.err.println("Error: Configuration file not found: " + args[0]);
+			System.exit(1);
 		}
-		
-		catch (FileNotFoundException fileNotFoundException) {
-			System.err.println("Error! FileNotFoundException caught");
+		catch (IOException e) {
+			System.err.println("Error: I/O exception while reading configuration file");
+			System.exit(1);
 		}
+		catch (Exception e) {
+			System.err.println("Error: Unexpected exception occurred: " + e.getMessage());
+			System.exit(1);
+		}
+	}
 
-		catch (IOException ioException) {
-			System.err.println("Error! IOException caught");
-		}
-
-		catch (Exception exception) {
-			System.err.println("Error! Exception caught");
-		}
+	/**
+	 * Prints usage information for the application.
+	 */
+	private static void printUsage() {
+		System.out.println("Usage: java -jar war-game-simulation.jar <config-file>");
+		System.out.println("  config-file: Path to the game configuration file");
 	}
 }
--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -2,51 +2,52 @@
  * @author Felipe Mardones
  * @version 1.0
  * 
- * True driver of the war game simulation. Creates a window to display
- * the game to the human player, and allows for interaction.
+ * True driver of the war game simulation. Manages the game state and player interactions.
+ *
+ * TODO: Migrate from deprecated Applet to JavaFX for GUI rendering when web UI is planned
  */
 
 package app;
 
-import java.applet.*;
 import java.util.concurrent.*;
 
 import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import javax.xml.bind.JAXBException;
 
-import model.map.Board;
+public class MainApp implements Runnable {
 
-@SuppressWarnings("serial")
-public class MainApp extends Applet implements Runnable {
+	// TODO: Initialize board when game is fully implemented
+	// private Board map;
 
-	Board map;
-
-	public MainApp(String fileName) throws FileNotFoundException, IOException, JAXBException, InterruptedException {
-		super();
-
+	/**
+	 * Initializes the main application with a configuration file.
+	 *
+	 * @param fileName Path to the configuration file
+	 * @throws FileNotFoundException if the configuration file is not found
+	 * @throws IOException if an I/O error occurs
+	 */
+	public MainApp(String fileName) throws FileNotFoundException, IOException {
 		File file = new File(fileName);
-		if (file.exists()) {
-			return;
+		if (!file.exists()) {
+			throw new FileNotFoundException("Configuration file not found: " + fileName);
 		}
+
+		// TODO: Load configuration from file
+		// TODO: Initialize game state
 	}
 
-	public void init() {
-		super.init();
-		this.run();
-	}
-
-
+	@Override
 	public void run() {
-		Semaphore sem = new Semaphore(1);
+		// TODO: Implement game loop
+		// Semaphore sem = new Semaphore(1);
 
-		Player player1 = new Player(sem);
-		Player player2 = new Player(sem);
+		// TODO: Create Player class
+		// Player player1 = new Player(sem);
+		// Player player2 = new Player(sem);
 
-		player1.start();
-		player2.start();
-		
+		// player1.start();
+		// player2.start();
 	}
 }
